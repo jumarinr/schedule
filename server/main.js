15176c69a062381c5f8472f44995fe38e334c3fe
Juan Diego Marín Rodríguez <jumarinr@unal.edu.co>
2020-05-15 03:23:30 -0500

    Ajuste

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ import { Meteor } from "meteor/meteor";
 import collections from "./../imports/collections";
 import api from "./../imports/api";
 
+
 // const { Users } = collections;
 
 Meteor.startup(() => {
